56613f8e33a822a153e4909509a2c69f832e0749
Steven Das <steven.c.das@gmail.com>
2015-10-07 19:51:00 -0500

    vertex and edge objects

--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -2,6 +2,20 @@
 
 namespace Graph
 {
+	public class Vertex
+	{
+		public Vertex ()
+		{
+		}
+	}
+
+	public class Edge
+	{
+		public Edge ()
+		{
+		}
+	}
+
 	public class Graph
 	{
 		public Graph ()
