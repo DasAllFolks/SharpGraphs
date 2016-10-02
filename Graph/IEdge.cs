a86762a5ccfa017f79a8d8a67e1898df182d49c4
Steven Das <steven.c.das@gmail.com>
2016-10-02 00:39:55 -0700

    Add IEdge interface

---
+++ Graph/IEdge.cs
@@ -0,0 +1,13 @@
+﻿namespace Graph
+{
+    /// <summary>
+    /// Represents an undirected edge (link) in a <see cref="Graph"/> using
+    /// vertex (node) labels of type T.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type used to create vertex (node) labels.
+    /// </typeparam>
+    public interface IEdge<T>
+    {
+    }
+}
