05dd4a85e02aedcc57ab132ffe391ccd5ad79358
Steven Das <steven.c.das@gmail.com>
2016-10-02 20:30:47 -0700

    Scaffold out weighted edge

---
+++ Graph/IWeightedEdge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Graph
+{
+    /// <summary>
+    /// Represent an 
+    /// </summary>
+    /// <typeparam name="V">
+    /// The type used to create vertex (node) labels.
+    /// </typeparam>
+    /// <typeparam name="W">
+    /// The type used for the edge weight.
+    /// </typeparam>
+    public interface IWeightedEdge<V, W> : IEdge<V>
+        where V : struct, IEquatable<V>
+        where W : struct, IEquatable<W>
+    {
+    }
+}
