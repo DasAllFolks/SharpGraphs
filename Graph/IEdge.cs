f3dace6ce21b9a4c0dd404e883d854774af9c248
Steven Das <steven.c.das@gmail.com>
2016-10-02 21:09:10 -0700

    Make all IEdges weighted

--- Graph/IEdge.cs
+++ Graph/IEdge.cs
@@ -8,8 +8,16 @@ namespace Graph
     /// <typeparam name="V">
     /// The type used to create vertex (node) labels.
     /// </typeparam>
-    public interface IEdge<V> : IEquatable<IEdge<V>>
+    /// <typeparam name="W">
+    /// The type used for the edge weight.
+    /// </typeparam>
+    public interface IEdge<V, W> : IEquatable<IEdge<V, W>>
         where V : struct, IEquatable<V>
+        where W : struct, IEquatable<W>, IComparable<W>
     {
+        /// <summary>
+        /// The weight of the <see cref="IEdge{V, W}"/>.
+        /// </summary>
+        W Weight { get; set; }
     }
 }
