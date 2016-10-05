d506e405acdc21ba542928ee8f0c4c16142b024d
Steven Das <steven.c.das@gmail.com>
2016-10-04 20:56:17 -0700

    Finish implementing AbstractEdge.

--- Graph/AbstractEdge.cs
+++ Graph/AbstractEdge.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Graph
 {
@@ -16,9 +17,30 @@ namespace Graph
         where V : struct, IEquatable<V>
         where W : struct, IComparable<W>, IEquatable<W>
     {
+        public IReadOnlyCollection<V> Vertices { get; private set; }
+
         /// <summary>
         /// The weight of the <see cref="AbstractEdge{V, W}"/>.
         /// </summary>
         public W Weight { get; set; }
+
+        /// <summary>
+        /// Populate the <see cref="AbstractEdge{V, W}"/>'s vertices and
+        /// <see cref="Weight"/>.
+        /// </summary>
+        /// <param name="vertex1">
+        /// The <see cref="AbstractEdge{V, W}"/>'s first vertex.
+        /// </param>
+        /// <param name="vertex2">
+        /// The <see cref="AbstractEdge{V, W}"/>'s second vertex.
+        /// </param>
+        /// <param name="weight">
+        /// The <see cref="AbstractEdge{V, W}"/>'s weight.
+        /// </param>
+        public AbstractEdge(V vertex1, V vertex2, W weight)
+        {
+            Vertices = new List<V> { vertex1, vertex2 };
+            Weight = weight;
+        }
     }
 }
